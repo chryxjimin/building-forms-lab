eb744bd47ee5684d54ab545f4918a43d794cd2ec
Your Username <you@example.com>
2021-05-05 20:38:19 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,50 @@
 // Add BandInput component
 import React, { Component } from 'react'
 
+
 class BandInput extends Component {
+  state = {
+    name: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: '',
+    });
+  };
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <p>
+            <input type="text"
+              onChange={event => this.handleChange(event)}
+              value={this.state.name}
+            />
+            <input type="submit" />
+          </p>
+        </form>
       </div>
     )
   }
 }
 
 export default BandInput
+
+
+//We would like to update this component's state 
+// each time the user types a new character in the input, 
+// stored as this.state.name. We will only update the Redux 
+// store's state with prop addBand, which calls dispatch when the user hits the submit button. 
+// The dispatched action required to initiate this 
+// update should be set up in BandsContainer and passed 
+// down as the prop addBand to BandInput.
